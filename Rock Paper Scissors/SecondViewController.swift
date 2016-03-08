b399f95cc2c7b8c7b0e11a819f02fb100669bd9e
Edwin Vergara <heydot.at@gmail.com>
2016-03-08 14:18:58 -0600

    Add Game Logic

--- Rock Paper Scissors/SecondViewController.swift
+++ Rock Paper Scissors/SecondViewController.swift
@@ -9,30 +9,57 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    var userChoice: RPSChoices?
+    var computerChoice: RPSChoices?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewWillAppear(animated: Bool) {
+        self.compareChoices(userChoice, secondChoice: computerChoice)
     }
     
     @IBAction func dismiss() {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func compareChoices(firstChoice: RPSChoices?, secondChoice: RPSChoices?) {
+        let total = firstChoice!.rawValue + secondChoice!.rawValue
+        let winMessage = " You Win!"
+        let loseMessage = " You Lose!"
+        
+        func appendMessage(winChoice: RPSChoices) {
+            if userChoice == winChoice {
+                label.text?.appendContentsOf(winMessage)
+            } else {
+                label.text?.appendContentsOf(loseMessage)
+            }
+        }
+        
+        if firstChoice == secondChoice {
+            label.text = "It's a tie"
+            imageView.image = UIImage(named: "itsATie")
+        } else {
+            switch total {
+            case 1:
+                label.text = "Paper Covers Rock."
+                imageView.image = UIImage(named: "PaperCoversRock")
+                appendMessage(.Paper)
+            case 2:
+                label.text = "Rock Crushes Scissors."
+                imageView.image = UIImage(named: "RockCrushesScissors")
+                appendMessage(.Rock)
+            case 3:
+                label.text = "Scissors Cut Paper."
+                imageView.image = UIImage(named: "ScissorsCutPaper")
+                appendMessage(.Scissors)
+            default:
+                print("user \(userChoice!) computer\(secondChoice!)")
+            }
+        }
+        
     }
-    */
+    
 
 }
--- Rock Paper Scissors/ViewController.swift
+++ Rock Paper Scissors/ViewController.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+enum RPSChoices: Int {
+    case Rock
+    case Paper
+    case Scissors
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -15,18 +21,35 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     @IBAction func chooseRock() {
         let controller = self.storyboard?.instantiateViewControllerWithIdentifier("SecondViewController") as! SecondViewController
+        controller.userChoice = RPSChoices.Rock
+        controller.computerChoice = randomChoice()
         presentViewController(controller, animated: true, completion: nil)
     }
 
     @IBAction func choosePaper() {
         performSegueWithIdentifier("paperSegue", sender: self)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let controller = segue.destinationViewController as! SecondViewController
+        
+        let isScissorsSegue = segue.identifier == "scissorsSegue"
+        let isPaperSegue = segue.identifier == "paperSegue"
+        if isScissorsSegue {
+            controller.userChoice = RPSChoices.Scissors
+            controller.computerChoice = randomChoice()
+        } else if isPaperSegue {
+            controller.userChoice = RPSChoices.Paper
+            controller.computerChoice = randomChoice()
+        }
+    }
+    
+    func randomChoice() -> RPSChoices {
+        let randomInt = Int( arc4random() % 3 )
+        let choice = RPSChoices.init(rawValue: randomInt)!
+        return choice
     }
 }
 
