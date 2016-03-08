8eb68eaf2f6f492ffb3ba7424a315f451837c218
Edwin Vergara <heydot.at@gmail.com>
2016-03-08 12:53:52 -0600

    Initial Commit

---
+++ Rock Paper Scissors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rock Paper Scissors
+//
+//  Created by Edwin Vergara on 3/8/16.
+//  Copyright © 2016 Edwin Vergara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
